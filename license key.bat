ef7e7e1c69d06b3d8b4a697317725d1a7ccfc349
Maksim32124 <100491876+Maksim32124@users.noreply.github.com>
2022-02-26 22:51:05 +0200

    code License
    3123123131232132427438219439173921749813749821748321748774873243274321443535412341324234168732463872916374687321648726487326487321648732148216348732648732164873264863287421648736482163482168746387421648726387462138746287462173621635931231878347521938751976387217t7326875873683216487126432172187r6174263458732174817534571328746287571298429865918753872570175298u40951887159818419755731275393218741874727432474721471o8258727531752768715675765611157632873777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777777

---
+++ license key.bat
@@ -0,0 +1,12 @@
+@echo off
+color 02
+echo H78J9-56G32-K54GD-H8909 H78J9-56G32-K54GD-H8909 >>license_key.txt
+:v
+color 04
+title %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random%
+echo H78J9-56G32-K54GD-H8909 H78J9-56G32-K54GD-H8909 H78J9-56G32-K54GD-H8909 H78J9-56G32-K54GD-H8909 H78J9-56G32-K54GD-H8909
+echo %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random%
+echo H78J9-56G32-K54GD-H8909 H78J9-56G32-K54GD-H8909 H78J9-56G32-K54GD-H8909 H78J9-56G32-K54GD-H8909 H78J9-56G32-K54GD-H8909
+echo  %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random%
+color 02 
+goto v
